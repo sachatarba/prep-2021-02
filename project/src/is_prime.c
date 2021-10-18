07b00e586cae35cede04b89eec02c0553b5f19b8
sachatarba <sachatarba@rambler.ru>
2021-10-18 17:23:42 +0300

    more code style changes

--- project/src/is_prime.c
+++ project/src/is_prime.c
@@ -2,14 +2,13 @@
 #include <stddef.h>
 
 
-
 int is_prime(size_t number) {
     if (number < 2) {
         return 0;
     }
     for (size_t i = 2; i < number; ++i) {
         if (number % i == 0) {
-        return 0;
+            return 0;
         }
     }
     return 1;
