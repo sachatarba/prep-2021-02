06751ff0dcbf627f4f354406874a3a596b8735b8
sachatarba <sachatarba@rambler.ru>
2021-10-24 15:44:18 +0300

    first

---
+++ project/include/masterRecord.h
@@ -0,0 +1,11 @@
+struct masterRecord{
+    int Number; 
+    char Name[20]; 
+    char Surname[20]; 
+    char addres[30]; 
+    char TelNumber[15]; 
+    double indebtedness; 
+    double credit_limit; 
+    double cash_payments; 
+};
+
